63663e3812660d70942fdaf8081917cb6ac37c5e
yuvraj6569 <yuvraj@techmapperz.com>
2025-04-11 17:59:29 +0530

    11-04-2025 05-59 Commit

--- app/_Component/GallerySection.js
+++ app/_Component/GallerySection.js
@@ -6,13 +6,13 @@ import { FaImage } from "react-icons/fa";
 const galleryImages = [
     "/gallery/1.webp",
     "/gallery/2.webp",
-    "/gallery/2.webp",
-    "/gallery/2.webp",
-    "/gallery/2.webp",
-    "/gallery/1.webp",
-    "/gallery/1.webp",
-    "/gallery/1.webp",
-    "/gallery/1.webp",
+    "/gallery/3.webp",
+    "/gallery/4.webp",
+    "/gallery/5.webp",
+    "/gallery/6.webp",
+    "/gallery/7.webp",
+    "/gallery/8.webp",
+    "/gallery/9.webp",
 ];
 
 const GallerySection = () => {
