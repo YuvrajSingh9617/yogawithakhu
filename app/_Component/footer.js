8be1c5f4e3e757c76fa2f7168f8f614bc2917ec2
yuvraj6569 <yuvraj@techmapperz.com>
2025-04-11 16:50:20 +0530

    11-04-2025 04-50 Commit

--- app/_Component/footer.js
+++ app/_Component/footer.js
@@ -15,19 +15,19 @@ const Footer = () => {
                     </p>
                     <div className="mt-4 space-y-2">
                         <p className="flex items-center gap-2">
-                            <Link href="tel:+918938990265" className="flex items-center gap-2 hover:no-underline">
+                            <Link rel="preload" href="tel:+918938990265" className="flex items-center gap-2 hover:no-underline">
                                 <FaPhoneAlt className="text-yellow-300" />
                                 +91-8938990265
                             </Link>
                         </p>
                         <p className="flex items-center gap-2">
-                            <Link href="mailto:akhileshraturi06@gmail.com" className="flex items-center gap-2 hover:no-underline">
+                            <Link rel="preload" href="mailto:akhileshraturi06@gmail.com" className="flex items-center gap-2 hover:no-underline">
                                 <FaEnvelope className="text-yellow-300" />
                                 akhileshraturi06@gmail.com
                             </Link>
                         </p>
                         <p className="flex items-center gap-2">
-                            <Link href="https://yogawithakhu.vercel.app" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 hover:no-underline">
+                            <Link rel="preload" href="https://yogawithakhu.vercel.app" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 hover:no-underline">
                                 <FaGlobe className="text-yellow-300" />
                                 yogawithakhu.vercel.app
                             </Link>
@@ -40,17 +40,17 @@ const Footer = () => {
                     <h3 className="text-lg font-semibold text-gray-300">Useful Links</h3>
                     <ul className="mt-2 space-y-2">
                         <li>
-                            <Link href="#home"
+                            <Link rel="preload" href="#home"
                                 className="hover:no-underline cursor-pointer">Home
                             </Link>
                         </li>
                         <li>
-                            <Link href="#about"
+                            <Link rel="preload" href="#about"
                                 className="hover:no-underline cursor-pointer">About Us
                             </Link>
                         </li>
                         <li>
-                            <Link href="#contact"
+                            <Link rel="preload" href="#contact"
                                 className="hover:no-underline cursor-pointer">Contact Us
                             </Link>
                         </li>
@@ -62,12 +62,12 @@ const Footer = () => {
                     <h3 className="text-lg font-semibold text-gray-300">Our Services</h3>
                     <ul className="mt-2 space-y-2">
                         <li>
-                            <Link href="/#quality">
+                            <Link rel="preload" href="/#quality">
                                 <span className="hover:no-underline cursor-pointer">Quality</span>
                             </Link>
                         </li>
                         <li>
-                            <Link href="/#products">
+                            <Link rel="preload" href="/#products">
                                 <span className="hover:no-underline cursor-pointer">Products</span>
                             </Link>
                         </li>
@@ -80,7 +80,7 @@ const Footer = () => {
                 <p>Copyright &copy; 2025 Yoga Center. All Rights Reserved</p>
                 <p className="mt-1">
                     Developed by{" "}
-                    <Link href="https://Yuvraj_Singh" target="_blank">
+                    <Link rel="preload" href="https://Yuvraj_Singh" target="_blank">
                         <span className="text-blue-400 hover:no-underline cursor-pointer">Yuvraj Singh</span>
                     </Link>
                 </p>
